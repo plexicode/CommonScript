6d56b5a1f4d7297606e04da4cb244490c1485a9b
Blake O'Hare <mspaintmaestro@gmail.com>
2024-09-11 09:49:46 +0900

    Surface more fields from Pastel through to TaskResult in the .NET harness.

--- dotnetharness/CommonScriptRuntime/TaskResult.cs
+++ dotnetharness/CommonScriptRuntime/TaskResult.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommonScript.Runtime.Internal;
+using System;
 
 namespace CommonScript.Runtime
 {
+    public enum TaskResultStatus
+    {
+        DONE,
+        ERROR,
+        SLEEP,
+        SUSPEND,
+    }
+
     public class TaskResult
     {
         private object resObj;
         internal TaskResult(object resObj)
         {
             this.resObj = resObj;
+        }
+
+        public int SleepMillis
+        {
+            get
+            {
+                return FunctionWrapper.PUBLIC_getTaskResultSleepAmount(this.resObj);
+            }
+        }
+
+        public string[] ErrorMessage
+        {
+            get
+            {
+                return FunctionWrapper.PUBLIC_getTaskResultError(this.resObj, true);
+            }
+        }
+
+        public TaskResultStatus Status
+        {
+            get
+            {
+                int status = FunctionWrapper.PUBLIC_getTaskResultStatus(this.resObj);
+                switch (status)
+                {
+                    case 1: return TaskResultStatus.DONE;
+                    case 2: return TaskResultStatus.ERROR;
+                    case 3: return TaskResultStatus.SUSPEND;
+                    case 4: return TaskResultStatus.SLEEP;
+                }
+                throw new NotImplementedException();
+            }
         }
     }
 }
